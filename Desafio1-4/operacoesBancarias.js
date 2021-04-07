66a4991c95c4e148313dccc463bd1a45764446f1
Karen Cardoso <kah.card@gmail.com>
2021-04-07 17:19:53 -0300

    update and finish operacoesBancarias.js

--- Desafio1-4/operacoesBancarias.js
+++ Desafio1-4/operacoesBancarias.js
@@ -1,23 +1,72 @@
 const user = {
-    name: "Mariana",
-    transactions: [],
-    balance: 0
-  };
+  name: "Mariana",
+  transactions: [],
+  balance: 0
+};
 
-  function createTransaction(transaciton){
-    user.transactions.push(transaction)
+function createTransaction(transaction) {
+  user.transactions.push(transaction)
 
-      if(transaciton.type == 'credit'){
-        user.balance += transation.value
-      }
-      else{
-        user.balance -= transaction.value
-      }
+  if (transaction.type == 'credit') {
+    user.balance += transaction.value
   }
-  
-createTransaction({ type: "credit", value: 50 });
-createTransaction({ type: "credit", value: 120 });
-createTransaction({ type: "debit", value: 80 });
-createTransaction({ type: "debit", value: 30 });
+  else {
+    user.balance -= transaction.value
+  }
+}
 
-  
+function getHigherTransactionByType(type) { // { type: 'credit', value: 120 }
+
+  let higherTransaction
+  let higherValue = 0
+
+  for (let transaction of user.transactions) {
+    if (type == transaction.type && higherValue < transaction.value) {
+      higherValue = transaction.value
+      higherTransaction = transaction
+    }
+  }
+  return higherTransaction
+}
+
+function getAverageTransactionValue() {
+  let sum = 0
+  for (let transaction of user.transactions) {
+    sum += transaction.value
+  }
+  return sum / user.transactions.length
+}
+
+function getTransactionsCount(){
+  let countCredit = 0
+  let countDebit = 0
+
+  for(let transaction of user.transactions){
+    if (transaction.type == 'credit') {
+      countCredit++
+    }
+    else{
+      countDebit++
+    }
+  }
+
+  return{
+    credit: countCredit,
+    debit: countDebit
+  }
+
+}
+
+createTransaction({ type: 'credit', value: 50 });
+createTransaction({ type: 'credit', value: 120 });
+createTransaction({ type: 'debit', value: 80 });
+createTransaction({ type: 'debit', value: 30 });
+
+console.log(user.balance); // 60
+
+console.log(getHigherTransactionByType('credit')) // { type: 'credit', value: 120 }
+console.log(getHigherTransactionByType('debit')) // { type: 'debit', value: 80 }
+
+console.log(getAverageTransactionValue()) // 70
+
+console.log(getTransactionsCount()) // { credit: 2, debit: 2 }
