3af4a7ce5332565057e0292117068c99bbf2b0f4
Karen Cardoso <kah.card@gmail.com>
2021-04-07 12:29:32 -0300

    update file operacoesBancarias.js

--- Desafio1-4/operacoesBancarias.js
+++ Desafio1-4/operacoesBancarias.js
@@ -4,9 +4,20 @@ const user = {
     balance: 0
   };
 
-  function createTransaction(){
-      
+  function createTransaction(transaciton){
+    user.transactions.push(transaction)
+
+      if(transaciton.type == 'credit'){
+        user.balance += transation.value
+      }
+      else{
+        user.balance -= transaction.value
+      }
   }
   
+createTransaction({ type: "credit", value: 50 });
+createTransaction({ type: "credit", value: 120 });
+createTransaction({ type: "debit", value: 80 });
+createTransaction({ type: "debit", value: 30 });
 
   
